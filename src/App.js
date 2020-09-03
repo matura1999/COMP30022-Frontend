608953edc77ecfddc6dfcb4757cb909a4ed2d44b
tiiikJackie <jackie.wang@tiiik.com.au>
2020-09-03 17:23:05 +0800

    main page completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './components/header';
+import Intro from './pages/intro'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Hello World
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      
+      <Router>
+        <Header/>
+        <div className="App">
+          <Switch>
+            <Route exact path='/'
+              exact component = {Intro}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
