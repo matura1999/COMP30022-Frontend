0d51b01734cbd02f4c150a5636740d220c334629
tiiikJackie <jackie.wang@tiiik.com.au>
2020-09-03 12:09:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const favicon = require('express-favicon')
 const path = require('path');
+const { static } = require('express');
 const port = process.env.port || 3000;
 const app = express();
+app.use(favicon(__dirname + '/build/favicon/ico'));
+app.use(express/static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
