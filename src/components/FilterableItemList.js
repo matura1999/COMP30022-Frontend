2509bb76e757438e333928b266674181176aa3f3
matura1999 <yipengpeter@gmail.com>
2020-09-19 21:06:44 +1000

    Feature: sort by Name

---
+++ src/components/FilterableItemList.js
@@ -0,0 +1,101 @@
+import React from "react";
+import FileItem from "./FileItem";
+import "../styles/UserCentre/component/FilterableItemList.css";
+
+class FileList extends React.Component {
+  render() {
+    const filterText = this.props.filterText;
+    const sortMethod = this.props.sortMethod;
+    const rows = [];
+
+    const listAfterSearch = this.props.files.filter(({ name }) =>
+      name.toLowerCase().includes(filterText.toLowerCase())
+    );
+
+    if (sortMethod === "byName") {
+      listAfterSearch.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    listAfterSearch.forEach(({ name, type, date, size }) => {
+      rows.push(<FileItem name={name} type={type} date={date} size={size} />);
+    });
+
+    return <div className="fileList">{rows}</div>;
+  }
+}
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleFilterTextChange = (e) => {
+    this.props.onFilterTextChange(e.target.value);
+  };
+
+  handleSortChange = (e) => {
+    this.props.onSortChange(e.target.value);
+  };
+
+  render() {
+    return (
+      <form className="searchAndSortBar row d-flex justify-content-between">
+        <input
+          className="searchBar"
+          type="text"
+          placeholder="Search File"
+          value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
+        />
+
+        <select className="sortBar" onChange={this.handleSortChange}>
+          <option value="byDefault">Sort By Default</option>
+          <option value="byName">Sort By Name</option>
+          {/* <option value="bySize">Sort By Size</option>
+          <option value="byDate">Sort By Date</option> */}
+        </select>
+      </form>
+    );
+  }
+}
+
+class FilterableItemList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: "",
+      sortMethod: "byDefault",
+    };
+  }
+
+  handleFilterTextChange = (filterText) => {
+    this.setState({
+      filterText: filterText,
+    });
+  };
+
+  handleSortChange = (sortMethod) => {
+    this.setState({
+      sortMethod: sortMethod,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          onFilterTextChange={this.handleFilterTextChange}
+          onSortChange={this.handleSortChange}
+        />
+        <FileList
+          files={this.props.files}
+          filterText={this.state.filterText}
+          sortMethod={this.state.sortMethod}
+        />
+      </div>
+    );
+  }
+}
+
+export default FilterableItemList;
--- src/pages/manageFiles.js
+++ src/pages/manageFiles.js
@@ -1,39 +1,39 @@
 import React, { Component } from "react";
-import FileItem from "../components/FileItem";
+import FilterableItemList from "../components/FilterableItemList";
 
 const fileItemList = [
   {
-    name: "File Name 1",
+    name: "File Name 1 Alice",
     type: "PDF",
     date: "2020-09-01 01:01:00",
     size: "2 MB",
   },
   {
-    name: "File Name 2",
+    name: "File Name 2 bob",
     type: "ZIP",
     date: "2020-09-01 01:01:00",
     size: "200 KB",
   },
   {
-    name: "File Name 3",
+    name: "File Name 3 Chris",
     type: "HTML",
     date: "2020-09-01 01:01:00",
     size: "10 MB",
   },
   {
-    name: "File Name 4",
+    name: "a File Name 4 hehe1",
     type: "CSS",
     date: "2020-09-01 01:01:00",
     size: "2 MB",
   },
   {
-    name: "File Name 5",
+    name: "x File Name 5 hehe2",
     type: "JS",
     date: "2020-09-01 01:01:00",
     size: "2 MB",
   },
   {
-    name: "File Name 6",
+    name: "File Name 6 he3",
     type: "DOC",
     date: "2020-09-01 01:01:00",
     size: "2 MB",
@@ -44,10 +44,7 @@ export default class ManageFiles extends Component {
   render() {
     return (
       <div className="fileList">
-        {fileItemList &&
-          fileItemList.map(({ name, type, date, size }) => (
-            <FileItem name={name} type={type} date={date} size={size} />
-          ))}
+        <FilterableItemList files={fileItemList} />
       </div>
     );
   }
