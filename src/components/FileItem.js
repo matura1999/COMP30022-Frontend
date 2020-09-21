0d34cf0e504a2b5e4eb118ff9ed855d586b7c50f
matura1999 <yipengpeter@gmail.com>
2020-09-21 17:39:06 +1000

    fixed some css bugs

--- src/components/FileItem.js
+++ src/components/FileItem.js
@@ -4,24 +4,25 @@ import "../styles/UserCentre/component/FileItem.css";
 
 const FileItem = ({ name, type, size, date }) => (
   <div className="container">
-    <div className="item row">
-      <div className="typeIconContainer col-2">
-        <div className="typeIcon d-flex justify-content-center align-items-center">
-          {type}
+    <div className="item">
+      <div className="iconAndInfo">
+        <div className="typeIconContainer">
+          <div className="typeIcon">{type}</div>
+        </div>
+
+        <div className="info">
+          <div className="title">{name}</div>
+
+          <div className="typeSizeContainer">
+            <div className="type">{type} File</div>
+            <div className="size">{size}</div>
+          </div>
+
+          <div className="date">{date}</div>
         </div>
       </div>
 
-      <div className="info col-7 d-flex flex-column">
-        <div className="title">{name}</div>
-
-        <div className="typeSizeContainer d-flex justify-content-between">
-          <div className="type">{type} File</div>
-          <div className="size">{size}</div>
-        </div>
-
-        <div className="date">{date}</div>
-      </div>
-      <div className="options col-3 d-flex flex-column justify-content-center align-items-end">
+      <div className="options">
         <button className="download">Download</button>
         <button className="delete">Delete</button>
       </div>
