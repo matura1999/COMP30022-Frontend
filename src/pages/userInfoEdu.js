001574a21f3f3cfd480e6df047db184658126ddc
Xiaohan Liu <xiaohanl4@student.unimelb.edu.au>
2020-09-22 10:49:27 +0800

    made work info and edu info page

--- src/pages/userInfoEdu.js
+++ src/pages/userInfoEdu.js
@@ -1,9 +1,80 @@
 import React, { Component } from "react";
+import { Form, Input, Button, Space, DatePicker, Row, Col, Select } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 
 export default class UserInfoEdu extends Component {
+    onFinish = (values) => {
+        console.log('Received values of form:', values);
+    };
+
     render() {
         return (
-            <span>Hello World</span>
+            <Form name="eduInfo" onFinish={this.onFinish} autoComplete="off">
+                <Form.List name="users">
+                    {(fields, { add, remove }) => {
+                        return (
+                            <div>
+                                {fields.map(field => (
+                                    <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="start">
+                                        <Row gutter={16}>
+                                            <Col span={8}>
+                                                <Form.Item
+                                                    name = "level"
+                                                    rules={[{ required: true, message: 'Please select level of education! ' }]}
+                                                >
+                                                    <Select>
+                                                        <Select.Option value="secondary">Secondary Education</Select.Option>
+                                                        <Select.Option value="subDegree">Sub-degree</Select.Option>
+                                                        <Select.Option value="bachelor">Bachelor</Select.Option>
+                                                        <Select.Option value="postgraduate">Postgraduate</Select.Option>
+                                                    </Select>
+                                                </Form.Item>
+                                            </Col>
+                                            <Col span={8}>
+                                                <Form.Item
+                                                    name="institution"
+                                                    rules={[{ required: true, message: 'Missing institution name' }]}
+                                                >
+                                                    <Input placeholder="Institution" />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col span={8}>
+                                                <Form.Item name="major">
+                                                    <Input placeholder="Major (optional)" />
+                                                </Form.Item>
+                                            </Col>
+                                        </Row>
+
+                                        <MinusCircleOutlined
+                                            onClick={() => {
+                                                remove(field.name);
+                                            }}
+                                        />
+                                    </Space>
+                                ))}
+
+                                <Form.Item>
+                                    <Button
+                                        type="dashed"
+                                        onClick={() => {
+                                            add();
+                                        }}
+                                        block
+                                    >
+                                        <PlusOutlined /> Add Record
+                                    </Button>
+                                </Form.Item>
+                            </div>
+                        );
+                    }}
+                </Form.List>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
         )
     }
 }
--- src/pages/userInfoWork.js
+++ src/pages/userInfoWork.js
@@ -1,9 +1,90 @@
 import React, { Component } from "react";
+import { Form, Input, Button, Space, DatePicker, Row, Col } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+
+const { RangePicker } = DatePicker;
 
 export default class UserInfoWork extends Component {
+    onFinish = (values) => {
+        console.log('Received values of form:', values);
+    };
+
+    rangeConfig = {
+        rules: [{ type: 'array', required: true, message: 'Please select time!' }],
+    };
+
     render() {
         return (
-            <span>Hello World</span>
+            <Form name="workInfo" onFinish={this.onFinish} autoComplete="off">
+                <Form.List name="users">
+                    {(fields, { add, remove }) => {
+                        return (
+                            <div>
+                                {fields.map(field => (
+                                    <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="start">
+                                        <Row gutter={16}>
+                                            <Col span={8}>
+                                            <Form.Item name="range-picker"  {...this.rangeConfig}>
+                                                <RangePicker />
+                                            </Form.Item>
+                                            </Col>
+                                            <Col span={8}>
+                                            <Form.Item
+                                                name="company"
+                                                rules={[{ required: true, message: 'Missing company name' }]}
+                                            >
+                                                <Input placeholder="Company" />
+                                            </Form.Item>
+                                            </Col>
+                                            <Col span={8}>
+                                            <Form.Item
+                                                name="job"
+                                                rules={[{ required: true, message: 'Missing job name' }]}
+                                            >
+                                                <Input placeholder="Job" />
+                                            </Form.Item>
+                                            </Col>
+                                        <Col span={24}>
+                                        <Form.Item
+                                            {...field}
+                                            name="job-desc"
+                                            rules={[{ required: true, message: 'Missing job description' }]}
+                                        >
+                                            <Input.TextArea placeholder="Description" />
+                                        </Form.Item>
+                                        </Col>
+                                        </Row>
+
+                                        <MinusCircleOutlined
+                                            onClick={() => {
+                                                remove(field.name);
+                                            }}
+                                        />
+                                    </Space>
+                                ))}
+
+                                <Form.Item>
+                                    <Button
+                                        type="dashed"
+                                        onClick={() => {
+                                            add();
+                                        }}
+                                        block
+                                    >
+                                        <PlusOutlined /> Add Record
+                                    </Button>
+                                </Form.Item>
+                            </div>
+                        );
+                    }}
+                </Form.List>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
         )
     }
 }
