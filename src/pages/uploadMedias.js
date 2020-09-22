f68ff3ee830fd87c9cac57f57f9788d9f3f8e7be
zixlin0828 <61874302+zixlin0828@users.noreply.github.com>
2020-09-22 21:21:17 +0800

    uploadmedia

--- src/pages/uploadMedias.js
+++ src/pages/uploadMedias.js
@@ -1,9 +1,149 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Upload, Button, message, Modal, Input } from 'antd';
+import {PlusOutlined, UploadOutlined} from '@ant-design/icons';
+import reqwest from 'reqwest';
+
+function getBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
 
 export default class UploadMedias extends Component {
+    state = {
+        fileList: [],
+        uploading: false,
+        previewVisible: false,
+        descriptionBox:false,
+        previewImage: '',
+        previewTitle: '',
+    };
+
+    handleCancel = () => this.setState({ previewVisible: false });
+
+    handlePreview = async file => {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj);
+        }
+
+        this.setState({
+            previewImage: file.url || file.preview,
+            previewVisible: true,
+            previewTitle: file.name || file.url.substring(file.url.lastIndexOf('/') + 1),
+        });
+    };
+
+    handleChange = ({ fileList }) => this.setState({ fileList , descriptionBox: true,},);
+
+    handleUpload = () => {
+        const { fileList } = this.state;
+        const formData = new FormData();
+        fileList.forEach(file => {
+            formData.append('files[]', file);
+        });
+
+        this.setState({
+            uploading: true,
+        });
+
+        // You can use any AJAX library you like
+        reqwest({
+            url: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+            method: 'post',
+            processData: false,
+            data: formData,
+            success: () => {
+                this.setState({
+                    fileList: [],
+                    uploading: false,
+                    descriptionBox: false,
+                });
+                message.success('upload successfully.');
+            },
+            error: () => {
+                this.setState({
+                    uploading: false,
+                });
+                message.error('upload failed.');
+            },
+        });
+    };
+
     render() {
+        const { previewVisible, descriptionBox, previewImage, previewTitle, uploading, fileList } = this.state;
+        const { TextArea } = Input;
+        const onChange = e => {
+            console.log(e);
+        };
+        let description
+        if (descriptionBox) {
+            description = (
+                <div>
+                    {/*<label htmlFor="description">Description:</label>*/}
+                    <br />
+                    <TextArea placeholder="Enter Description Here" allowClear onChange={onChange} rows={4} />
+                </div>)
+        }
+        const uploadButton = (
+            <div>
+                <PlusOutlined />
+                <div style={{ marginTop: 8 }}>Upload</div>
+            </div>
+        );
+        const props = {
+            onRemove: file => {
+                this.setState(state => {
+                    const index = state.fileList.indexOf(file);
+                    const newFileList = state.fileList.slice();
+                    newFileList.splice(index, 1);
+                    return {
+                        fileList: newFileList,
+                    };
+                });
+            },
+            beforeUpload: file => {
+                this.setState(state => ({
+                    fileList: [...state.fileList, file],
+                }));
+                return false;
+            },
+            fileList,
+        };
+
         return (
-            <span>Hello World</span>
-        )
+            <>
+                <Upload     props
+                            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                            listType="picture-card"
+                            fileList={fileList}
+                            onPreview={this.handlePreview}
+                            onChange={this.handleChange}>
+                    {uploadButton}
+                </Upload>
+                <Modal
+                    visible={previewVisible}
+                    title={previewTitle}
+                    footer={null}
+                    onCancel={this.handleCancel}
+                >
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
+                {description}
+                <Button
+                    type="primary"
+                    onClick={this.handleUpload}
+                    disabled={fileList.length === 0}
+                    loading={uploading}
+                    style={{ marginTop: 16 , backgroundColor: "#9ACD32"}}
+                >
+                    {uploading ? 'Uploading' : 'Start Upload'}
+                </Button>
+            </>
+        );
     }
+
 }
