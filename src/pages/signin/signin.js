3342be034ebd1284962e97567600eb09dc6cd3e6
matura1999 <yipengpeter@gmail.com>
2020-10-20 12:43:17 +1100

    finished uploading and deleting media API

--- src/pages/signin/signin.js
+++ src/pages/signin/signin.js
@@ -1,129 +1,144 @@
 import React, { Component } from "react";
-import { Alert, Form, Input, Button} from 'antd';
+import { Alert, Form, Input, Button } from "antd";
 import { Link } from "react-router-dom";
-import "./signIn.scss"
+import "./signIn.scss";
 const layout = {
-    labelCol: { offset: 2, span: 8 },
-    wrapperCol: { offset: 2, span: 20 },
+  labelCol: { offset: 2, span: 8 },
+  wrapperCol: { offset: 2, span: 20 },
 };
 const tailLayout = {
-    wrapperCol: { offset: 2, span: 20 },
+  wrapperCol: { offset: 2, span: 20 },
 };
 
 export default class SignIn extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            notice: ''
-        };
-
-        this.onFinish = this.onFinish.bind(this);
-        this.onFinishFailed = this.onFinishFailed.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      notice: "",
     };
 
+    this.onFinish = this.onFinish.bind(this);
+    this.onFinishFailed = this.onFinishFailed.bind(this);
+  }
 
-    onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
+  onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  onFinish = (values) => {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        username: values.username,
+        password: values.password,
+      }),
     };
-
-    onFinish = (values) => {
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify({ username: values.username, password: values.password })
-        };
-        fetch('https://mojito-portfolio-backend.herokuapp.com/user/find', requestOptions)
-            .then(res => res.json())
-            .then(res => {
-                console.log(res)
-                if (res.success === false) {
-                    setTimeout(() => {
-                        this.setState({
-                            notice: res.error,
-                        });
-                    }, 300);
-                } else {
-                    sessionStorage.setItem('authorised', true);
-                    sessionStorage.setItem('user', res.id);
-                    sessionStorage.setItem('username', res.username);
-                    window.location.href = "/";
-                }
-            })
-    };
-
-    render() {
-        if (!sessionStorage.getItem('authorised')) {
-            return (
-                <div class="signIn__body">
-                    <h2 class="signIn__title">
-                        <bold>
-                            Sign in to Mojito
-                        </bold>
-                    </h2>
-                    {this.state.notice && (
-                        <Alert
-                            style={{ alignSelf: 'center', width: '100' }}
-                            message={this.state.notice}
-                            type="error"
-                            showIcon
-                            closable
-                        />
-                    )}
-                    <div class="signIn__form">
-                        <Form
-                            {...layout}
-                            name="signin"
-                            layout="vertical"
-                            initialValues={{ remember: true }}
-                            onFinish={this.onFinish}
-                            onFinishFailed={this.onFinishFailed}
-                        >
-                            <Form.Item
-                                style={{ fontSize: '20px' }}
-                                label="Username or email address"
-                                name="username"
-                                rules={[{ required: true, message: 'Please input your username or your email address!' }]}
-                            >
-                                <Input />
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Password"
-                                name="password"
-                                rules={[{ required: true, message: 'Please input your password!' }]}
-                            >
-                                <Input.Password />
-                            </Form.Item>
-
-                            <Form.Item {...tailLayout}>
-                                <Button type="primary" htmlType="submit" size='large' block style={{ background: '#8CC63E' }}>
-                                    Sign in
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                    </div>
-
-
-                    <div class="signIn__redirectToSignUp">
-                        New to Mojito{"?   "}<Link exact to='/signup'><u>Create an account!</u></Link>
-                    </div>
-                </div>
-            )
+    fetch(
+      "https://mojito-portfolio-backend.herokuapp.com/user/find",
+      requestOptions
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        if (res.success === false) {
+          setTimeout(() => {
+            this.setState({
+              notice: res.error,
+            });
+          }, 300);
         } else {
-            return (
-                <div class="signIn__body">
-                    <h2 class="signIn__title">
-                        <bold>
-                            You have already signed in!
-                        </bold>
-                    </h2>
-                </div>
-            )
+          sessionStorage.setItem("authorised", true);
+          sessionStorage.setItem("user", res.id);
+          sessionStorage.setItem("username", res.username);
+          window.location.href = "/";
         }
-    }
+      });
+  };
 
+  render() {
+    if (!sessionStorage.getItem("authorised")) {
+      return (
+        <div class="signIn__body">
+          <h2 class="signIn__title">
+            <bold>Sign in to Mojito</bold>
+          </h2>
+          {this.state.notice && (
+            <Alert
+              style={{ alignSelf: "center", width: "100" }}
+              message={this.state.notice}
+              type="error"
+              showIcon
+              closable
+            />
+          )}
+          <div class="signIn__form">
+            <Form
+              {...layout}
+              name="signin"
+              layout="vertical"
+              initialValues={{ remember: true }}
+              onFinish={this.onFinish}
+              onFinishFailed={this.onFinishFailed}
+            >
+              <Form.Item
+                style={{ fontSize: "20px" }}
+                label="Username or email address"
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message:
+                      "Please input your username or your email address!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your password!" },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item {...tailLayout}>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  size="large"
+                  block
+                  style={{ background: "#8CC63E" }}
+                >
+                  Sign in
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+
+          <div class="signIn__redirectToSignUp">
+            New to Mojito{"?   "}
+            <Link exact to="/signup">
+              <u>Create an account!</u>
+            </Link>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div class="signIn__body">
+          <h2 class="signIn__title">
+            <bold>You have already signed in!</bold>
+          </h2>
+        </div>
+      );
+    }
+  }
 }
--- src/pages/userCentreManageContent/manageMedias.js
+++ src/pages/userCentreManageContent/manageMedias.js
@@ -1,62 +1,14 @@
 import React, { Component } from "react";
+import FilterableMediaList from "./components/filterableMediaList";
 import "./manageMedias.scss";
-import { Popconfirm, Popover, Card, Image, Tooltip, Input, Modal } from "antd";
-import {
-  EditOutlined,
-  ZoomInOutlined,
-  DeleteOutlined,
-} from "@ant-design/icons";
-import EditDescriptionModal from "./components/editDescriptionModal";
-import MediaItem from "./components/mediaItem";
-
-const mediaItemList = [
-  {
-    source:
-      "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
-    alt: "aa",
-    time: "2020-09-01 01:01:00",
-    description: "This is the description  jsndkjandkajsndajks",
-  },
-  {
-    source:
-      "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    alt: "a",
-    time: "2020-10-20 01:01:02",
-    description:
-      "This is the description ffffffffffffffffffffffffffffffffffffffffffffffffff",
-  },
-  {
-    source: "https://www.w3schools.com/images/w3schools_green.jpg",
-    alt: "b",
-    time: "2020-09-01 01:02:00",
-    description: "hahahhahha",
-  },
-  {
-    source: "https://www.w3schools.com/images/w3schools_green.jpg",
-    alt: "c",
-    time: "2020-09-01 01:01:00",
-    description: "This is the description",
-  },
-  {
-    source:
-      "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    alt: "d",
-    time: "2020-09-01 01:01:00",
-    description: "This is the description",
-  },
-  {
-    source:
-      "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    alt: "e",
-    time: "2020-10-20 01:01:00",
-    description: "This is the description",
-  },
-];
-
-class MediaList extends React.Component {
-  state = {
-    files: this.props.files,
-  };
+export default class manageMedias extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notice: "",
+      mediaItemList: [],
+    };
+  }
 
   componentDidMount = async () => {
     const requestOptions = {
@@ -82,87 +34,54 @@ class MediaList extends React.Component {
             });
           }, 300);
         } else {
-          console.log(res.data)
+          this.setState({
+            notice: res.message,
+          });
+          console.log(res.data);
+          const temMediaList = [];
+          res.data.map(
+            ({
+              url: mediaUrl,
+              LastModified: date,
+              description,
+              descriptionUrl,
+            }) => {
+              // const name = fileUrl.split("/").pop();
+              // const type = name.split(".").pop().toUpperCase();
+              // const kbSize = Math.round(size / 1024);
+              // let fileSize = "";
+              // if (kbSize <= 1024) {
+              //   fileSize = `${kbSize}KB`;
+              // } else {
+              //   const mbSize = Math.round(kbSize / 1024);
+              //   fileSize = `${mbSize}MB`;
+              // }
+              let dateObj = new Date(date);
+              const createdDate = dateObj.toLocaleString();
+              const mediaObject = {
+                time: createdDate,
+                description: description,
+                source: mediaUrl,
+                descriptionUrl: descriptionUrl,
+              };
+
+              temMediaList.push(mediaObject);
+            }
+          );
+          console.log(temMediaList);
+          this.setState({
+            mediaItemList: temMediaList,
+          });
         }
       });
   };
-
-  render() {
-    const filterText = this.props.filterText;
-    const rows = [];
-
-    const listAfterSearch = this.props.files.filter(({ description }) =>
-      description.toLowerCase().includes(filterText.toLowerCase())
-    );
-
-    listAfterSearch.sort((a, b) => b.time.localeCompare(a.time));
-
-    listAfterSearch.forEach(({ source, alt, time, description }, index) => {
-      rows.push(
-        <MediaItem
-          source={source}
-          alt={alt}
-          time={time}
-          description={description}
-        />
-      );
-    });
-
-    return <div className="manageMedia__content">{rows}</div>;
-  }
-}
-
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  handleFilterTextChange = (e) => {
-    this.props.onFilterTextChange(e.target.value);
-  };
-
-  render() {
-    return (
-      <form className="searchAndSortBar">
-        <input
-          className="manageMedia__searchBar"
-          type="text"
-          placeholder="Search Media"
-          value={this.props.filterText}
-          onChange={this.handleFilterTextChange}
-        />
-      </form>
-    );
-  }
-}
-
-export default class manageMedias extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      files: mediaItemList,
-      filterText: "",
-    };
-  }
 
   handleFilterTextChange = (filterText) => {
     this.setState({
       filterText: filterText,
     });
   };
-
   render() {
-    return (
-      <div>
-        <SearchBar
-          filterText={this.state.filterText}
-          onFilterTextChange={this.handleFilterTextChange}
-        />
-        <MediaList
-          files={this.state.files}
-          filterText={this.state.filterText}
-        />
-      </div>
-    );
+    return <FilterableMediaList medias={this.state.mediaItemList} />;
   }
 }
