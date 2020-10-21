9c3650dc309ff6ffb1cb73b9c82f43ce1ff14a55
Xiaohan Liu <xiaohanl4@student.unimelb.edu.au>
2020-10-21 21:16:08 +0800

    added app.js back

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Header from "./components/header/header";
+import Intro from "./pages/intro/intro";
+import SignUp from "./pages/signUp/signUp";
+import SignIn from "./pages/signIn/signIn";
+import UserCentre from "./pages/userCentre/userCentre";
+import UserPortfolio from "./pages/userPortfolio/userPortfolio";
+import PortfolioEssayDetailed from "./pages/userPortfolioEssays/essayDetailed";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchResult from "./pages/searchResult/searchResult";
+import "./App.css";
+import "antd/dist/antd.css";
+import "antd/dist/antd.less";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Header />
+          <div className="App-body">
+            <Switch>
+              <Route exact path="/" exact component={Intro} />
+              <Route exact path="/signUp" exact component={SignUp} />
+              <Route exact path="/signIn" exact component={SignIn} />
+              <Route path="/userCentre/:path/:subpath" component={UserCentre} />
+              <Route
+                path="/userPortfolio/:username/:path"
+                component={UserPortfolio}
+              />
+              <Route path="/searchResult" component={SearchResult} />
+              <Route exact
+                  path="/userPortfolio/:username/:path/:subpath"
+                  component={PortfolioEssayDetailed}
+              />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
