98a7721301160545c1a781d1d99cd48f3a7fcd8e
matura1999 <yipengpeter@gmail.com>
2020-10-25 15:53:09 +1100

    update thumbnail button

--- src/components/filterableEssayList/editEssayModal.js
+++ src/components/filterableEssayList/editEssayModal.js
@@ -1,7 +1,24 @@
-import { Button, Modal, Input } from "antd";
-import { EditOutlined } from "@ant-design/icons";
+import { Button, Modal, Input, Upload, message } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 import React, { Component } from "react";
 
+const props = {
+  name: "file",
+  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+  headers: {
+    authorization: "authorization-text",
+  },
+  onChange(info) {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
 export default class EditEssayModal extends React.Component {
   state = {
     // loading: false,
@@ -91,6 +108,14 @@ export default class EditEssayModal extends React.Component {
             rows={4}
             allowClear
           />
+          <Upload {...props}>
+            <Button
+              className="essayItem__uploadThumbnail"
+              icon={<UploadOutlined />}
+            >
+              Update Thumbnail
+            </Button>
+          </Upload>
         </Modal>
       </>
     );
