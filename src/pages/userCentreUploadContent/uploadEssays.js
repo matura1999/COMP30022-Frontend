93a18b2d8ce91e6f9607ef9b94074b384575b906
HaoxiangW <haoxiangw@student.unimelb.edu.au>
2020-10-25 22:07:57 +1100

    essay upload updated

--- src/pages/userCentreUploadContent/uploadEssays.js
+++ src/pages/userCentreUploadContent/uploadEssays.js
@@ -1,89 +1,103 @@
 import React, { Component } from "react";
-import {Upload, Button, Form, Input, Divider, Space} from "antd";
+import { Upload, Button, Form, Input, Divider, Space, message } from "antd";
 import "./uploadEssays.css";
+import reqwest from 'reqwest';
 
 
 export default class UploadEssays extends Component {
-    state = {
-        fileList: [
-        ],
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            fileList: [],
+        };
+    }
 
-    handleChange = info => {
-        let fileList = [...info.fileList];
 
-        // 1. Limit the number of uploaded files
-        // Only to show two recent uploaded files, and old ones will be replaced by the new
-        fileList = fileList.slice(-2);
+    beforeUpload = info => {
+        this.setState({ fileList: [info] });
+        return false;
+    }
 
-        // 2. Read from response and show file link
-        fileList = fileList.map(file => {
-            if (file.response) {
-                // Component will show file.url as link
-                file.url = file.response.url;
-            }
-            return file;
-        });
-
-        this.setState({ fileList });
+    handleRemove = () => {
+        this.setState({ fileList: [] })
     };
 
     onFinish = (values) => {
-        console.log(values);
+        const { fileList} = this.state;
+        const formData = new FormData();
+        fileList.forEach(file => {
+            formData.append('file', file);
+        });
+        
+        formData.append('username', sessionStorage.getItem('username'))
+        formData.append('title', values.title)
+        formData.append('content', values.body)
+        formData.append('date', new Date())
+        reqwest({
+            url: 'https://mojito-portfolio-backend.herokuapp.com/files/essay',
+            method: 'PUT',
+            processData: false,
+            data: formData,
+            success: () => {
+                this.setState({
+                    fileList: [],
+                });
+                message.success(`Uploaded successfully.`);
+            },
+
+            error: () => {
+                this.setState({
+                    uploading: false,
+                });
+                message.error('Upload failed.');
+            },
+        });
     };
 
     render() {
+        const {fileList} = this.state
         const props = {
-            action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-            onChange: this.handleChange,
-            multiple: true,
+            beforeUpload: this.beforeUpload,
+            onRemove: this.handleRemove,
+            fileList
         };
 
         return (
             <div className="essay_component">
 
-            <Form name="nest-messages" onFinish={this.onFinish} id="essay_component">
-                <Form.Item name='title' placeholder="Enter Title Here">
-                    <Input placeholder="Enter Title Here" bordered={false}/>
-                </Form.Item>
-                <Divider />
-                <Form.Item name='body' >
-                    <Input.TextArea rows={10} placeholder="Enter Content Here" bordered={false}/>
-                </Form.Item>
-                <Divider />
-                <Form.Item >
+                <Form name="nest-messages" onFinish={this.onFinish} id="essay_component">
+                    <Form.Item name='title' placeholder="Enter Title Here">
+                        <Input placeholder="Enter Title Here" bordered={false} />
+                    </Form.Item>
+                    <Divider />
+                    <Form.Item name='body' >
+                        <Input.TextArea rows={10} placeholder="Enter Content Here" bordered={false} />
+                    </Form.Item>
+                    <Divider />
                     <Upload {...props}
-                            fileList={this.state.fileList}
-                            restrictions={{
-                        allowedExtensions: [ '.jpg', '.png' ]
-                    }}>
-                        <Button htmlType="submit">
+                        showUploadList={true}
+                        accept=".jpg,.png,.jpeg"
+                    >
+                        <Button>
                             Upload Thumbnail
-                        </Button>
+                    </Button>
                     </Upload>
 
-                </Form.Item>
-                <Divider />
+                    <Divider />
 
-                <Form.Item>    <Space size={"middle"}>
-                    <Button className="userCentre__formButton"
+                    <Form.Item>    <Space size={"middle"}>
+                        <Button className="userCentre__formButton"
                             type="primary"
                             htmlType="submit"
                             size="large"
-                    >
-                       Submit Now
+                        >
+                            Submit Now
                     </Button>
-                    <Button className="userCentre__formButton"
-                            type="primary"
-                            htmlType="submit"
-                            size="large"
-                    >
-                        Save Draft
-                    </Button>  </Space>
-                </Form.Item>
+                    </Space>
+                    </Form.Item>
 
 
-            </Form>
+                </Form>
 
             </div>
         )
